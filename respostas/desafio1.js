e865694dea34cadbda8de6758aa1c06d13395638
cleitonrs <rscleitonrs@gmail.com>
2022-07-05 17:23:32 -0300

    done

---
+++ respostas/desafio1.js
@@ -0,0 +1,6 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+const employee1 = {
+    code: 10,
+    name: "João"
+};
---
+++ respostas/desafio1.ts
@@ -0,0 +1,13 @@
+
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+interface Employee {
+  code: number,
+  name: string
+
+} 
+
+const employee1: Employee = {
+  code: 10,
+  name: "João"
+}
